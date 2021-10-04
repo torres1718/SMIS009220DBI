a9e01a02aac74f9d9e7a684f2290f0441a394af7
torres1718 <cristobaltorres273@gmail.com>
2021-10-04 10:11:49 -0600

    Se añadió una vista para listadoProductos

--- practicaSemana11.sql
+++ practicaSemana11.sql
@@ -1,5 +1,28 @@
 USE Northwind;
 
+SELECT
+	p.ProductName Producto,
+	c.CategoryName Categoria
+FROM
+	Products p
+	INNER JOIN Categories c
+	ON p.CategoryID=c.CategoryID;
 
 
+--crear una vista que muestre nombre de producto
+--categoria y proveedor.
+--ordenado por nombre de categoria
 
+CREATE VIEW listadoProductos
+AS
+SELECT p.ProductName AS 'Producto',
+c.CategoryName AS 'Categoria',
+p.SupplierID AS 'Proveedores'
+FROM Products p
+INNER JOIN Categories c
+ON p.CategoryID=c.CategoryID
+
+SELECT Producto, Categoria, Proveedores FROM listadoProductos ORDER BY Categoria;
+
+
+ALTER VIEW
